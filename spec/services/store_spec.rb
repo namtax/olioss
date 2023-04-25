ea6e9ef542edf568e549191a9eac42b747d18a47
namtax <namtax@namtaxs-MBP.fritz.box>
2023-04-25 20:04:26 +0100

    Clean up spec

--- spec/services/store_spec.rb
+++ spec/services/store_spec.rb
@@ -7,60 +7,7 @@ RSpec.describe Store do
     subject                      { described_class.run(input) }
     let(:input)                  { file_fixture('articles.json').read }
     let(:article_attrs)          { Article.find(3899631).attributes.transform_values{ |v| v.is_a?(ActiveSupport::TimeWithZone) ? v.to_s : v } }
-    let(:expected_article_attrs) do 
-      {
-        "id"=>3899631, 
-        "title"=>"Ambipur air freshener plugin",
-        "description"=>"Device only but refills are available most places", 
-        "donation_description"=>"", 
-        "collection_id"=>0, 
-        "section"=>"product", 
-        "location" => {
-            "latitude" => 51.7645,
-            "longitude" =>  -3.79131,
-            "distance" => 0,
-            "town" => "Ystalyfera",
-            "country" => "United Kingdom"
-        }, 
-        "collection_notes"=> "Any time!", 
-        "value" => {
-            "price" => 0,
-            "currency" => "USD",
-            "payment_type" => "no_payment_type"
-        },
-        "status"=>"active", 
-        "expiry"=>"2021-04-09 10:49:15 UTC", 
-        "reactions" => {
-            "likes" => 0,
-            "by_user" => false,
-            "views" => 18,
-            "impressions" => 3571
-        },
-        "is_owner"=>true, 
-        "conversations" => {
-            "linked" => 0,
-            "request_id" => nil
-        },
-        "photos" => [
-            {
-                "uid" => "00gRGrBRDFYrR2j-9SJVYg",
-                "files" => {
-                    "original" => "https:\/\/cdn.olioex.com\/uploads\/photo\/file\/00gRGrBRDFYrR2j-9SJVYg\/image.jpg",
-                    "large" => "https:\/\/cdn.olioex.com\/uploads\/photo\/file\/00gRGrBRDFYrR2j-9SJVYg\/large_image.jpg",
-                    "medium" => "https:\/\/cdn.olioex.com\/uploads\/photo\/file\/00gRGrBRDFYrR2j-9SJVYg\/medium_image.jpg",
-                    "small" => "https:\/\/cdn.olioex.com\/uploads\/photo\/file\/00gRGrBRDFYrR2j-9SJVYg\/small_image.jpg"
-                },
-                "dimensions" => {
-                    "width" => 720,
-                    "height" => 960
-                }
-            }
-        ],
-        "user_id"=>8039, 
-        "created_at"=>"2020-12-12 10:49:18 UTC", 
-        "updated_at"=>"2020-12-12 10:49:18 UTC",
-      }
-    end
+    let(:expected_article_attrs) { JSON.load(file_fixture('article_attrs.json').read) }
 
     context 'articles table empty' do
       it 'persists article to database' do 
